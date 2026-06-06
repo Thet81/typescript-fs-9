addb39131d113a724ac225d29f7599c8e426eab9
thetnaing <thetnaingoo1530@gmail.com>
2026-06-06 15:04:49 +0630

    Adding express to the mix

--- ch3/multiplier.ts
+++ ch3/multiplier.ts
@@ -5,7 +5,7 @@ interface MultiplyValues {
 }
 
 const parseArguments = (args:string[]): MultiplyValues => {
-  if (args.length < 4) throw new Error("Not enought arguments");
+  if (args.length < 4) throw new Error("Not enough arguments");
   if (args.length > 4) throw new Error("Too many arguments");
 
   if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){

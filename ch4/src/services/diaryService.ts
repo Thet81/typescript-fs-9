093f894a157ffc5266cbae6908c34e0d4d65f289
thetnaing <thetnaingoo1530@gmail.com>
2026-06-17 15:10:51 +0630

    defining types

---
+++ ch4/src/services/diaryService.ts
@@ -0,0 +1,15 @@
+
+import diaryData from '../../data/entries.json' with {type : "json"};
+
+const getEntries = ()=> {
+  return diaryData;
+};
+
+const addDiary = ()=> {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary
+};
